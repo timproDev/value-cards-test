fec41be820ff2558e277585eea51efdde76bfe76
Tim Schletter <schletterdesign@gmail.com>
2023-04-17 21:37:21 -0400

    public path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/value-cards-test/',
+  publicPath: '',
   plugins: [vue()],
   resolve: {
     alias: {
